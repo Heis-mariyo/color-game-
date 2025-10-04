14cdb8ff753e30e13238fe679db684f4cbd3177e
Heis-mariyo <yoyoizunobi@gmail.com>
2025-10-04 17:32:52 +0100

    Updated project files

---
+++ new/app.js
@@ -0,0 +1,12 @@
+import PixelBlast from "./components/fluideffect";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Fluid Effect Demo</h1>
+      <PixelBlast />
+    </div>
+  );
+}
+
+export default App;
